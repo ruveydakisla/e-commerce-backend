76cae1ff8fea51f848b87cccf1242ef1dd7862a8
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 19:23:43 +0300

    feat: create BaseEntity.ts, BaseEntityWithName.ts, order-item.entity.ts and order.entity.ts/orders entities created

---
+++ orders-microservice/src/orders/entities/order-item.entity.ts
@@ -0,0 +1,21 @@
+import { BaseEntity, Column, Entity, ManyToOne } from 'typeorm';
+import { Order } from './order.entity';
+@Entity('order_items')
+export class OrderItem extends BaseEntity {
+  productId: number;
+
+  @ManyToOne(() => Order, (order) => order.items, { onDelete: 'CASCADE' })
+  order: Order;
+  @Column({ type: 'int', default: 1 })
+  quantity: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalPrice: number;
+
+  constructor(dto: Partial<OrderItem>) {
+    super();
+    Object.assign(this, { ...dto });
+  }
+}
---
+++ orders-microservice/src/orders/entities/order.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Column, Entity, OneToMany } from 'typeorm';
+import { OrderItem } from './order-item.entity';
+@Entity('orders')
+export class Order extends BaseEntity {
+  userId: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalPrice: number;
+
+  @OneToMany(() => OrderItem, (orderItem: OrderItem) => orderItem.order, {
+    cascade: true,
+  })
+  items: OrderItem[];
+
+  constructor(base: Partial<BaseEntity>) {
+    super();
+    Object.assign(this, { ...base });
+  }
+}
