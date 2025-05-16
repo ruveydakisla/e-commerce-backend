f26a0311998a398e3962eb6e8a90e2fa081f9250
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 22:47:47 +0300

    update app.module.ts

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ProductsModule, OrdersModule],
+  imports: [UsersModule, AuthModule, ProductsModule, OrdersModule, CartModule],
   controllers: [],
   providers: [],
 })
