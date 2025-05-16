d33386d490241702522d0bd30d735549fa7547c8
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 18:53:49 +0300

    update app.module.ts

--- products-microservice/src/app.module.ts
+++ products-microservice/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('PRODUCTS_POSTGRES_HOST'),
+        port: config.get<number>('PRODUCTS_POSTGRES_PORT'),
+        username: config.get<string>('PRODUCTS_POSTGRES_USER'),
+        password: config.get<string>('PRODUCTS_POSTGRES_PASSWORD'),
+        database: config.get<string>('PRODUCTS_POSTGRES_DB'),
+        synchronize: true,
+        autoLoadEntities: true,
+        entities: [__dirname + '/**/entities/*.entity.ts'],
+        logging: true,
+        cache: { duration: config.get<number>('TYPEORM_CACHE_DURATION') },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
