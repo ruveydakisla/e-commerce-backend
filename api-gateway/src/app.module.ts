56a565bfc8d27e2047cacc631e8b11b4d7fa22fe
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 19:00:00 +0300

    update app.module.ts and main.ts

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ProductsModule],
   controllers: [],
   providers: [],
 })
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // class-transformer'ı da aktifleştirir
+    }),
+  );
   console.log('API GATEWAY 3000 portunda çalışıyor...');
 }
 bootstrap();
