b3c2cdd600a3bc5cfb0d3a150ff2877135542619
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 22:15:27 +0300

    create orders.controller.spec.ts, orders.controller.ts, orders.service.spec.ts and orders.service.ts: orders rest endpoints created

---
+++ api-gateway/src/orders/orders.service.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { ORDERS_PATTERNS } from './utils/types';
+
+@Injectable()
+export class OrdersService {
+  constructor(
+    @Inject('ORDERS_MICROSERVICE') private orderMicroservice: ClientProxy,
+  ) {}
+  create(createOrderDto: CreateOrderDto) {
+    return this.orderMicroservice.send(
+      { cmd: ORDERS_PATTERNS.Create },
+      createOrderDto,
+    );
+  }
+
+  findAll() {
+    return this.orderMicroservice.send({ cmd: ORDERS_PATTERNS.FindAll }, {});
+  }
+
+  findOne(id: number) {
+    return this.orderMicroservice.send({ cmd: ORDERS_PATTERNS.FindOne }, id);
+  }
+
+  update(id: number, updateOrderDto: UpdateOrderDto) {
+    return this.orderMicroservice.send(
+      { cmd: ORDERS_PATTERNS.Update },
+      { id, updateOrderDto },
+    );
+  }
+
+  remove(id: number) {
+    return this.orderMicroservice.send({ cmd: ORDERS_PATTERNS.Remove }, id);
+  }
+}
