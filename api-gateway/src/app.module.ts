631b343e43a0d347c65c95a5c3dd1b0e52b893e9
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 22:14:30 +0300

    update app.module.ts

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ProductsModule],
+  imports: [UsersModule, AuthModule, ProductsModule, OrdersModule],
   controllers: [],
   providers: [],
 })
