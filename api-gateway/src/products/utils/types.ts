737a8f025489a9a915d73c24275797d0c27cbda5
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 18:55:51 +0300

    feat: create types.ts

---
+++ api-gateway/src/products/utils/types.ts
@@ -0,0 +1,8 @@
+
+export const PRODUCTS_PATTERNS = {
+  FindAll: 'Products.FindAll',
+  FindOne: 'Products.FindOne',
+  Create: 'Products.Create',
+  Update: 'Products.Update',
+  Remove: 'Products.Remove',
+};
