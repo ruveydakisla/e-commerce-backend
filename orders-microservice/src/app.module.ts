34633f8e0cfa0b894434d73a2453a6971371f19a
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 19:25:39 +0300

    update app.module.ts: db config done

--- orders-microservice/src/app.module.ts
+++ orders-microservice/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('ORDERS_POSTGRES_HOST'),
+        port: config.get<number>('ORDERS_POSTGRES_PORT'),
+        username: config.get<string>('ORDERS_POSTGRES_USER'),
+        password: config.get<string>('ORDERS_POSTGRES_PASSWORD'),
+        database: config.get<string>('ORDERS_POSTGRES_DB'),
+        synchronize: true,
+        autoLoadEntities: true,
+        entities: [__dirname + '/**/entities/*.entity.ts'],
+        logging: true,
+        cache: { duration: config.get<number>('TYPEORM_CACHE_DURATION') },
+      }),
+    }),
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
