df27f1c5b8931123b4ebc9a4713a717723756a0b
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 22:35:34 +0300

    update app.module.ts :mongoose conifig added

--- cart-microservice/src/app.module.ts
+++ cart-microservice/src/app.module.ts
@@ -1,9 +1,44 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CartModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        connectionFactory: (connection) => {
+          connection.on('connected', () => {
+            console.log('MongoDB Bağlantısı Kuruldu');
+          });
+
+          connection.on('disconnected', () => {
+            console.warn('MongoDB Bağlantısı Kesildi');
+          });
+
+          connection.on('error', (error) => {
+            console.error('MongoDB Bağlantı Hatası:', error);
+          });
+
+          connection.on('reconnected', () => {
+            console.log('MongoDB Bağlantısı Yeniden Kuruldu');
+          });
+
+          connection.on('reconnectFailed', () => {
+            console.error('MongoDB Bağlantısı Yeniden Kurulamadı');
+          });
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
