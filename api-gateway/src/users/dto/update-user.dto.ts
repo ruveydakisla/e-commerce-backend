3ff40bc8692f1cd83dcef70995486d0f1ea4666a
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 14:56:35 +0300

    update update-user.dto.ts

--- api-gateway/src/users/dto/update-user.dto.ts
+++ api-gateway/src/users/dto/update-user.dto.ts
@@ -1,4 +1,29 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsOptional, IsStrongPassword } from 'class-validator';
+import { UserRole } from '../utils/types';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  name: string;
+  @IsOptional()
+  @IsEmail()
+  email: string;
+  @IsOptional()
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'Password is not strong enough, it should have min 8 char, at least 1 upper case, 1 lower case, 1 number and 1 symbol',
+    },
+  )
+  password: string;
+  @IsOptional()
+  birthdate: Date;
+  @IsOptional()
+  role: UserRole;
+}
