3b7f789dc3c59c4805cfe8f66fd3a1e56fdfd178
ruveyda <ruveydakisla34@gmail.com>
2025-05-16 22:05:20 +0300

    update orders.module.ts: product and users microservices imported

--- orders-microservice/src/orders/orders.module.ts
+++ orders-microservice/src/orders/orders.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
@@ -6,7 +7,22 @@ import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderItem]),
+
+    ClientsModule.register([
+      {
+        name: 'USERS_MICROSERVICE',
+        transport: Transport.TCP,
+        options: { port: 3020, host: 'users-microservice' },
+      },
+      {
+        name: 'PRODUCTS_MICROSERVICE',
+        transport: Transport.TCP,
+        options: { port: 3022, host: 'products-microservice' },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
