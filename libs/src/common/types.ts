a537df5a777b9b4c97543d982971c584ce201716
ruveyda <ruveydakisla34@gmail.com>
2025-05-21 23:52:48 +0300

    build(deps): update package-lock.json, package.json, types.ts and tsconfig.tsbuildinfo: libs updated

--- libs/src/common/types.ts
+++ libs/src/common/types.ts
@@ -6,3 +6,18 @@ export type PaginationOptions = {
   sort: string;
   order: SortOrder;
 };
+export interface PaginatedResult<T> {
+  data: T[];
+  total: number;
+  page: number;
+  limit: number;
+}
+export interface ApiResponse<T> {
+  success: boolean;
+  timestamp: string;
+  data: T;
+}
+export interface ApiErrorResponse {
+  success: boolean;
+  timestamp: string;
+}
