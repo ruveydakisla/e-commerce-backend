1cb1b5abde8ae9d95c7ec8f2888211971d476cd5
ruveyda <ruveydakisla34@gmail.com>
2025-05-24 12:32:32 +0300

    update products.module.ts : kanfa imported

--- products-microservice/src/products/products.module.ts
+++ products-microservice/src/products/products.module.ts
@@ -1,4 +1,6 @@
+import { SERVICES } from '@my/common';
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductImage } from './entities/product-image.entity';
 import { Product } from './entities/product.entity';
@@ -6,7 +8,20 @@ import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, ProductImage]),],
+  imports: [
+    TypeOrmModule.forFeature([Product, ProductImage]),
+    ClientsModule.register([
+      {
+        name: SERVICES.KAFKA.name,
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: [`${SERVICES.KAFKA.host}:${SERVICES.KAFKA.port}`],
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
