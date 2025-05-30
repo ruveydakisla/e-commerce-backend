33a2e18d70441be7801f21702d75208c2c4ddf87
ruveyda <ruveydakisla34@gmail.com>
2025-05-30 16:33:06 +0300

    guards updated

--- api-gateway/src/users/users.controller.ts
+++ api-gateway/src/users/users.controller.ts
@@ -14,7 +14,6 @@ import {
   Patch,
   Post,
   Query,
-  Req,
   UseGuards,
 } from '@nestjs/common';
 import { Roles } from 'src/auth/decorator/roles.decorator';
@@ -22,39 +21,30 @@ import { JwtAuthGuard } from 'src/auth/guards/JwtAuth.guard';
 import { OwnerOrRolesGuard } from 'src/auth/guards/owner-or-roles.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { UsersService } from './users.service';
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
+  @Roles(UserRole.SUPER_ADMIN)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
-  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.SUPER_ADMIN)
   @Get()
   findAll(@Query() query: PaginationOptions) {
     return this.usersService.findAll(query);
   }
-  @Get('me')
-  @UseGuards(JwtAuthGuard)
-  findMe(@Req() req) {
-    const sub = req.user.sub;
-    if (!sub) {
-      return { message: 'User not found' };
-    }
-    return this.usersService.findOne(sub);
-  }
 
+  @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   @Get(':id')
-  @UseGuards(JwtAuthGuard)
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
-  @UseGuards(JwtAuthGuard, OwnerOrRolesGuard)
-  @Roles(UserRole.SUPER_ADMIN)
+  @UseGuards(OwnerOrRolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
@@ -62,8 +52,8 @@ export class UsersController {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   @Delete(':id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.SUPER_ADMIN, UserRole.SELLER)
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(id);
