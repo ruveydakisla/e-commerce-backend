e0d44f7add4c0f4a1748c1bac831ed5abb7ab383
ruveyda <ruveydakisla34@gmail.com>
2025-05-30 16:33:20 +0300

    me endpoint added

--- api-gateway/src/auth/auth.controller.ts
+++ api-gateway/src/auth/auth.controller.ts
@@ -1,13 +1,29 @@
 import { LoginDto } from '@my/common';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/JwtAuth.guard';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Post('login')
   async login(@Body() lodinDto: LoginDto) {
     return this.authService.login(lodinDto);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  findMe(@Req() req) {
+    const sub = req.user.sub;
+    console.log(req.user);
+
+    if (!sub) {
+      return { message: 'User not found' };
+    }
+    return this.usersService.findOne(sub);
   }
 }
