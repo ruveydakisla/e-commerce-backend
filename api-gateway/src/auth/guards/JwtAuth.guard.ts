8fd70589a771edab740dccea177973f0d73efa92
ruveyda <ruveydakisla34@gmail.com>
2025-05-30 15:47:17 +0300

    verify moved JwtAuthGuard

--- api-gateway/src/auth/guards/JwtAuth.guard.ts
+++ api-gateway/src/auth/guards/JwtAuth.guard.ts
@@ -1,18 +1,30 @@
-import { AUTH_PATTERNS } from '@my/common';
+import { SERVICES } from '@my/common';
 import {
   CanActivate,
   ExecutionContext,
   Inject,
   Injectable,
 } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { ClientProxy } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(
-    @Inject('AUTH_MICROSERVICE') private readonly authMicroservice: ClientProxy,
+    private readonly jwtService: JwtService,
   ) {}
+
+  async verify(token: string) {
+    try {
+      console.log('auth microservice verify token', token);
+      const verified = await this.jwtService.verify(token);
+      return verified;
+    } catch (e) {
+      console.log('jwt verify hatası', e);
+
+      return { message: 'jwt verify hatası', error: e };
+    }
+  }
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
 
@@ -22,9 +34,7 @@ export class JwtAuthGuard implements CanActivate {
 
     const token = authHeader.replace('Bearer ', '');
     try {
-      const user = await firstValueFrom(
-        this.authMicroservice.send({ cmd: AUTH_PATTERNS.verify }, token),
-      );
+      const user = await this.verify(token);
 
       if ((req.user = user)) return true;
       else {
