0784d363d1fc87b248d136b794d4961b8e5fe94f
ruveyda <ruveydakisla34@gmail.com>
2025-06-10 12:52:03 +0300

    winston logger added

--- products-microservice/src/products/products.module.ts
+++ products-microservice/src/products/products.module.ts
@@ -2,6 +2,8 @@ import { SERVICES } from '@my/common';
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
+import { winstonLoggerConfig } from './common/logger';
 import { ProductImage } from './entities/product-image.entity';
 import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
@@ -10,6 +12,7 @@ import { ProductsService } from './products.service';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Product, ProductImage]),
+    WinstonModule.forRoot(winstonLoggerConfig),
     ClientsModule.register([
       {
         name: SERVICES.KAFKA.name,
