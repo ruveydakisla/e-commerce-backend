fd11c26b8c0c49b3bafae25fc164167150c26ce6
ruveyda <ruveydakisla34@gmail.com>
2025-06-13 00:34:42 +0300

    update product.entity.ts

--- products-microservice/src/products/entities/product.entity.ts
+++ products-microservice/src/products/entities/product.entity.ts
@@ -1,10 +1,12 @@
+import { BaseEntityWithName } from '@my/common';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { ProductImage } from './product-image.entity';
-import { BaseEntityWithName } from '@my/common';
 @Entity('products')
 export class Product extends BaseEntityWithName {
   @Column({ type: 'varchar', length: 255, unique: true })
   description: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  category: string;
 
   @Column({ type: 'decimal', precision: 10, scale: 2 })
   price: number;
@@ -18,7 +20,10 @@ export class Product extends BaseEntityWithName {
 
   @Column({ name: 'is_active', type: 'boolean', default: false })
   isActive: boolean;
-  @OneToMany(() => ProductImage, (productImage) => productImage.product)
+  @OneToMany(() => ProductImage, (productImage) => productImage.product, {
+    cascade: ['insert'],
+    eager: true,
+  })
   images: ProductImage[];
 
   constructor(productDTO: Partial<Product>) {
